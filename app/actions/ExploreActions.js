14894ff7a156ae81b8c2bc48111947788f2645bc
Zung-Ying Shen <colin8930@gmail.com>
2016-05-23 14:52:04 +0800

    use GET to query the image from mongoDB

--- app/actions/ExploreActions.js
+++ app/actions/ExploreActions.js
@@ -4,8 +4,27 @@ import alt from '../alt';
 class ExploreActions {
   constructor() {
     this.generateActions(
-      'exploreSuccess'
+      'exploreSuccess',
+      'exploreFail'
     );
+  }
+
+  exploreByClass(image_id) {
+
+    var jqXHR = $.ajax({
+      type: 'GET',
+      url: '/api/explore',
+      contentType: "application/json",
+      async: false,
+    });
+    console.log(jqXHR.responseJSON);
+    return jqXHR.responseJSON;
+    /*.done((data) => {
+        this.actions.exploreSuccess(data);
+      })
+    .fail((jqXhr) => {
+        this.actions.exploreFail(jqXhr.responseJSON.message);
+      });*/
   }
 }
 
