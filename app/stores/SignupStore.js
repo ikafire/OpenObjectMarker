644020fa856c65bb697b24c6998db9272fd877e3
Colin <colin8930@gmail.com>
2016-06-13 16:10:17 +0800

    fix signup store

--- app/stores/SignupStore.js
+++ app/stores/SignupStore.js
@@ -1,8 +1,46 @@
 import alt from '../alt';
+import SignupActions from '../actions/SignupActions';
 
 class SignupStore {
   constructor() {
-    this.user = '';
+    this.bindActions(SignupActions);
+    this.name = '';
+    this.email = '';
+    this.username = '';
+    this.password = '';
+    this.signupValidationState = '';
+  }
+
+  onSignupSuccess(successMessage) {
+    this.signupValidationState = 'has-success';
+  }
+
+  onAddLabelFail(errorMessage) {
+    this.signupValidationState = 'has-error';
+  }
+  
+  onUpdateName(event) {
+    this.name = event.target.value;
+    this.signupValidationState = '';
+  }
+
+  onUpdateEmail(event) {
+    this.email = event.target.value;
+    this.signupValidationState = '';
+  }
+
+  onUpdateUsername(event) {
+    this.username = event.target.value;
+    this.signupValidationState = '';
+  }
+
+  onUpdatePassword(event) {
+    this.password = event.target.value;
+    this.signupValidationState = '';
+  }
+
+  onInvalidLabel() {
+    this.signupValidationState = 'has-error';
   }
 }
 
